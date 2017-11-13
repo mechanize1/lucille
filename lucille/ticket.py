d90e6247eff2c00bece21c478dd3a82636c5f88e
fdiaz <fdiaz1500@gmail.com>
2017-11-13 01:23:52 -0500

    Add Flask functionality

--- lucille/ticket.py
+++ lucille/ticket.py
@@ -1,3 +1,27 @@
 """
-A services that listens for incoming service requests from tenets
+Customer-facing services that listens for incoming service requests from tenets.  Messages come in two forms:
+
+Message system
+    1.  General inquiries sent to our email
+Ticket system
+    1.  Specific inquiries with the problem
+        - We provide a menu of choices:  toilet, tub, sink, etc.
+        - The user snaps a photo, and leaves a description of the issue
+        - We respond with acknowledgment and 24-hour notice.
+Methodology:
+    - Poll for incoming connections from tenet
+
 """
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    # Containers to hold description and photos
+    desc = []
+    photo = []
+    return 'Hello world!'
+
+if __name__ == '__main__':
+    main()
